87ae2fb46caa1786e3108627cb81c0659862516e
William Iommi <william.iommi@gmail.com>
2021-12-31 17:04:58 +0100

    minor on clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,25 +2,31 @@ import { useEffect, useState } from "react";
 
 const getTime = () => {
   var date = new Date();
-  const format = n => n < 10 ? `0${n}` : n;
+  const format = (n) => (n < 10 ? `0${n}` : n);
   var h = format(date.getHours()); // 0 - 23
   var m = format(date.getMinutes());
   return `${h}:${m}`;
-}
+};
 
 const Clock = ({ clockSize }) => {
   const [time, setTime] = useState();
 
   useEffect(() => {
     setTime(getTime());
-    const timer = setInterval(() => {
+    const interval = setInterval(() => {
       setTime(getTime());
     }, 1000);
 
-    return () => clearTimeout(timer);
+    return () => clearInterval(interval);
   }, []);
 
-  return <div className={`${clockSize} relative flex items-center justify-end w-full text-white text-sm`}>{time}</div>;
+  return (
+    <div
+      className={`${clockSize} relative flex items-center justify-end w-full text-white text-sm`}
+    >
+      {time}
+    </div>
+  );
 };
 
 export default Clock;
