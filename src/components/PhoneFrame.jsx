5c2b24781bc16ee14d1c7a71176f4330e6158809
William Iommi <william.iommi@gmail.com>
2021-12-31 16:34:10 +0100

    notch section moved outside

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -1,18 +1,14 @@
 const PhoneFrame = ({ children, pro }) => {
   const frameSize = pro ? "h-[926px] w-[428px]" : "h-[712px] w-[350px]";
-  const notchSize = pro ? 'w-52' : 'w-40';
   return (
     <div
       className={`${frameSize} relative overflow-hidden border-[14px] border-black bg-black rounded-[60px] shadow-xl ring ring-orange-400 backdrop-blur-md`}
     >
       <img
-          src="/bg.png"
-          alt="bg"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
-        <div className="absolute top-0 inset-x-0">
-          <div className={`${notchSize} relative h-6 mx-auto bg-black rounded-b-3xl z-50`}></div>
-        </div>
+        src="/bg.png"
+        alt="bg"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
       {children}
     </div>
   );
