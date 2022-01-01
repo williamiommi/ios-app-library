3815df2445ec9725c044353bea708cc8b420b1d2
William Iommi <william.iommi@gmail.com>
2022-01-01 17:31:21 +0100

    App structure update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import PhoneFrame from "./components/PhoneFrame";
 import BackdropLayer from "./components/BackdropLayer";
+import FolderWrapper from "./components/FolderWrapper";
+import InputSearch from "./components/InputSearch";
+import TopBar from "./components/TopBar";
 
 function App() {
+  const [pro, setPro] = useState(false);
   return (
-    <div className="grid min-h-screen bg-purple-50 place-items-center">
-      <PhoneFrame>
-        <BackdropLayer />
-      </PhoneFrame>
-    </div>
+    <>
+      <div className="grid min-h-screen place-items-center">
+        <button onClick={() => setPro(!pro)}>pro</button>
+        <PhoneFrame pro={pro}>
+          <TopBar pro={pro} />
+          <BackdropLayer />
+          <InputSearch />
+          <FolderWrapper />
+        </PhoneFrame>
+      </div>
+    </>
   );
 }
 
