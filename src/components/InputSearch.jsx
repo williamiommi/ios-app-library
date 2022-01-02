a1e0b42db95f2c2fe77e8ec9062c843d6523edef
William Iommi <william.iommi@gmail.com>
2022-01-02 23:43:37 +0100

    minor

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -3,9 +3,9 @@ import Lens from "./icons/Lens";
 
 const InputSearch = () => {
   return (
-    <div className="relative z-20 flex items-end w-full h-[85px] pb-2 overflow-hidden bg-transparent backdrop-blur-sm">
+    <div className="relative z-20 flex items-end w-full h-[100px] pb-4 overflow-hidden bg-transparent backdrop-blur-sm">
       <div className="flex items-center w-full mx-5">
-        <div className="flex justify-center items-center w-full h-10 bg-gray-700/90 rounded-2xl">
+        <div className="flex justify-center items-center w-full h-12 bg-gray-700/90 rounded-2xl">
           <div className="absolute flex items-center transition-all duration-500 left-1/2 translate-x-[-50%]">
             <Lens />
             <span className="inline text-gray-500 w-full text-sm ml-1">
