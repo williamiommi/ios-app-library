ade5c8acd3e13cd6cca146f8268715eb2e9f1c97
William Iommi <william.iommi@gmail.com>
2022-01-02 03:45:02 +0100

    initial impl of the animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import PhoneFrame from "./components/PhoneFrame";
 import BackdropLayer from "./components/BackdropLayer";
 import FolderWrapper from "./components/FolderWrapper";
 import InputSearch from "./components/InputSearch";
 import TopBar from "./components/TopBar";
+import FolderDetail from "./components/FolderDetail";
+
+const appLibraryVariants = {
+  visible: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0, scale: 0.8, transition: { duration: 0.7 } },
+};
 
 function App() {
   const [pro, setPro] = useState(false);
+  const [openFolder, setOpenFolder] = useState(null);
   return (
     <>
       <div className="grid gap-10 place-items-center">
         <button onClick={() => setPro(!pro)}>pro</button>
         <PhoneFrame pro={pro}>
-          <TopBar pro={pro} />
           <BackdropLayer />
-          <InputSearch />
-          <FolderWrapper />
+          <TopBar pro={pro} />
+          <motion.div
+            animate={openFolder ? "hidden" : "visible"}
+            variants={appLibraryVariants}
+            className="h-full"
+          >
+            <InputSearch />
+            <FolderWrapper openFolderCB={setOpenFolder} />
+          </motion.div>
+          <FolderDetail folder={openFolder} />
         </PhoneFrame>
       </div>
     </>
--- src/components/FolderDetail.jsx
+++ src/components/FolderDetail.jsx
@@ -1,30 +1,76 @@
-import Icon from "./Icon";
+import { motion, useAnimation } from "framer-motion";
 import SimpleBar from "simplebar-react";
+import Icon from "./Icon";
 import useFolderDetailBlur from "../hooks/useFolderDetailBlur";
+import { useEffect } from "react";
+
+const variants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.7 } },
+  hidden: { opacity: 0, scale: 0.75, transition: { duration: 0.7 } },
+};
 
 const FolderDetail = ({ folder }) => {
-  const { blurredRef, titleRef } = useFolderDetailBlur();
+  const { blurredRef } = useFolderDetailBlur(folder);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const animate = async () => {
+      if (folder) {
+        controls.set(variants.hidden);
+        controls.start(variants.visible);
+      }
+    };
+    animate();
+  }, [folder, controls]);
+
   if (!folder) return null;
   return (
-    <div className="relative h-full">
+    <motion.div
+      animate={controls}
+      variants={variants}
+      className="absolute inset-0 h-full opacity-0"
+    >
       <SimpleBar className="!absolute flex flex-col justify-center items-center z-20 inset-0 pt-[20%]">
         <div
           ref={blurredRef}
-          className="sticky w-full top-0 z-30 h-[90px]"
+          className="invisible sticky w-full top-[-1px] z-30 h-[90px] backdrop-blur-md"
         ></div>
-        <h2
-          ref={titleRef}
-          className="sticky top-[50px] w-full text-white text-3xl mb-10 tracking-wide z-40 px-5"
-        >
+        <h2 className="sticky top-[50px] w-full text-white text-3xl mb-10 tracking-wide z-40 px-5">
           {folder.name}
         </h2>
         <div className="grid grid-cols-4 gap-3 px-5 pb-5">
           {folder.icons.map((icon) => (
-            <Icon key={icon.name} src={icon.src} text={icon.name} />
+            <>
+              <Icon
+                key={icon.name + Math.random()}
+                src={icon.src}
+                text={icon.name}
+              />
+              <Icon
+                key={icon.name + Math.random()}
+                src={icon.src}
+                text={icon.name}
+              />
+              <Icon
+                key={icon.name + Math.random()}
+                src={icon.src}
+                text={icon.name}
+              />
+              <Icon
+                key={icon.name + Math.random()}
+                src={icon.src}
+                text={icon.name}
+              />
+              <Icon
+                key={icon.name + Math.random()}
+                src={icon.src}
+                text={icon.name}
+              />
+            </>
           ))}
         </div>
       </SimpleBar>
-    </div>
+    </motion.div>
   );
 };
 
