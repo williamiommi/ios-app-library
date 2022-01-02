a4eb12dd014c377382caec6ddfaa96a9ef2c8425
William Iommi <william.iommi@gmail.com>
2022-01-02 01:57:29 +0100

    folder detail updat with custom hook

--- src/components/FolderDetail.jsx
+++ src/components/FolderDetail.jsx
@@ -1,28 +1,13 @@
 import Icon from "./Icon";
 import SimpleBar from "simplebar-react";
-import { useEffect, useRef } from "react";
+import useFolderDetailBlur from "../hooks/useFolderDetailBlur";
 
-const FolderDetail = ({ text, icons, show }) => {
-  const blurredRef = useRef();
-  const titleRef = useRef();
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([e]) => {
-        blurredRef.current.classList.toggle(
-          "backdrop-blur-md",
-          e.intersectionRatio < 1
-        );
-      },
-      {
-        threshold: [1],
-        rootMargin: "-140px",
-      } /* blurredRed h + titleRef top*/
-    );
-    observer.observe(titleRef.current);
-  }, []);
+const FolderDetail = ({ folder }) => {
+  const { blurredRef, titleRef } = useFolderDetailBlur();
+  if (!folder) return null;
   return (
     <div className="relative h-full">
-      <SimpleBar className="!absolute flex flex-col justify-center items-center z-20 inset-0 pt-[40%]">
+      <SimpleBar className="!absolute flex flex-col justify-center items-center z-20 inset-0 pt-[20%]">
         <div
           ref={blurredRef}
           className="sticky w-full top-0 z-30 h-[90px]"
@@ -31,10 +16,10 @@ const FolderDetail = ({ text, icons, show }) => {
           ref={titleRef}
           className="sticky top-[50px] w-full text-white text-3xl mb-10 tracking-wide z-40 px-5"
         >
-          {text}
+          {folder.name}
         </h2>
-        <div className="grid grid-cols-4 gap-3 px-5">
-          {icons.map((icon) => (
+        <div className="grid grid-cols-4 gap-3 px-5 pb-5">
+          {folder.icons.map((icon) => (
             <Icon key={icon.name} src={icon.src} text={icon.name} />
           ))}
         </div>
