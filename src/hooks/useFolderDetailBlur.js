36d31dde2db115b39b7a05e12c18659421f229ef
William Iommi <william.iommi@gmail.com>
2022-01-03 04:19:30 +0100

    minor

--- src/hooks/useFolderDetailBlur.js
+++ src/hooks/useFolderDetailBlur.js
@@ -3,9 +3,10 @@ import { useEffect, useRef } from "react";
 const useFolderDetailBlur = (folder) => {
   const blurredRef = useRef();
   useEffect(() => {
-    if (!folder || !blurredRef.current) return null;
+    if (!folder && !blurredRef.current) return null;
     const observer = new IntersectionObserver(
       ([e]) => {
+        if (!blurredRef.current) return;
         blurredRef.current.classList.toggle(
           "!visible",
           e.intersectionRatio < 1
