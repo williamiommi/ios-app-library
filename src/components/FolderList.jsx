a94799b4a6eb9f5d37fc1af7e86e96c2b93a0ab9
William Iommi <william.iommi@gmail.com>
2022-01-03 19:11:20 +0100

    no results added

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -16,16 +16,22 @@ const FolderList = () => {
     >
       <BackdropLayer level="xl" />
       {filteredApps && filteredApps.length === 0 ? (
-        <p className="absolute inset-0 flex items-center justify-center text-xl text-white">No Results</p>
+        <p className="absolute inset-0 flex items-center justify-center text-xl text-white">
+          No Results
+        </p>
       ) : (
-        <SimpleBar className="!absolute inset-0 pb-10 w-full max-h-full">
-          <div className="sticky top-[0] left-0 w-full h-[115px] backdrop-blur-sm z-30" />
-          {filteredApps && <FolderListEntry apps={filteredApps} />}
-          {!filteredApps &&
-            Object.keys(appsDict).map((key) => (
-              <FolderListEntry key={key} char={key} apps={appsDict[key]} />
-            ))}
-        </SimpleBar>
+        <>
+          <SimpleBar className="!absolute inset-0 pb-10 w-full max-h-full">
+            <div className="sticky top-[0] left-0 w-full h-[115px] backdrop-blur-sm z-30" />
+            {filteredApps && <FolderListEntry apps={filteredApps} />}
+            {!filteredApps &&
+              Object.keys(appsDict).map((key) => (
+                <>
+                  <FolderListEntry key={key} char={key} apps={appsDict[key]} />
+                </>
+              ))}
+          </SimpleBar>
+        </>
       )}
     </motion.div>
   );
