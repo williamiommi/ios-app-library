1af0e8af05f3d590c938480c32d7e1ea889cbcfa
William Iommi <william.iommi@gmail.com>
2022-01-04 00:04:45 +0100

    minor on folder wrapper

--- src/components/FolderWrapper.jsx
+++ src/components/FolderWrapper.jsx
@@ -4,18 +4,16 @@ import { useAppStateContext } from "../context/app";
 import Folder from "./Folder";
 
 function FolderWrapper() {
-  const { folders } = useAppStateContext();
+  const { folders, isFolderListOpen } = useAppStateContext();
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-[95px] backdrop-blur-sm z-20" />
+      {!isFolderListOpen && (
+        <div className="absolute top-0 left-0 w-full h-[95px] backdrop-blur-sm z-20" />
+      )}
       <SimpleBar className="!absolute top-0 p-5 pt-28 w-full max-h-full">
         <div className="grid grid-cols-2 gap-5">
           {folders.map((folder) => (
-            <Folder
-              key={folder.name}
-              icons={folder.icons}
-              name={folder.name}
-            />
+            <Folder key={folder.name} icons={folder.icons} name={folder.name} />
           ))}
         </div>
       </SimpleBar>
