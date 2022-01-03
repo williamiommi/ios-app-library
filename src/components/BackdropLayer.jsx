e51510fdaa53e4b1c4b368f8509e98df9271f087
William Iommi <william.iommi@gmail.com>
2022-01-03 03:31:38 +0100

    update backdroplayer

--- src/components/BackdropLayer.jsx
+++ src/components/BackdropLayer.jsx
@@ -1,8 +1,22 @@
 import { memo } from "react";
 
-const BackdropLayer = ({ children }) => {
+const BackdropLayer = ({ level, children }) => {
+  let backdropLevel = "backdrop-blur-md";
+  switch (level) {
+    case "md":
+      backdropLevel = "backdrop-blur-sm";
+      break;
+    case "lg":
+      backdropLevel = "backdrop-blur-lg";
+      break;
+    case "xl":
+      backdropLevel = "backdrop-blur-xl";
+      break;
+    default:
+      break;
+  }
   return (
-    <div className="absolute inset-0 bg-black/20 backdrop-blur-md">
+    <div className={`absolute inset-0 bg-black/20 ${backdropLevel}`}>
       {children}
     </div>
   );
