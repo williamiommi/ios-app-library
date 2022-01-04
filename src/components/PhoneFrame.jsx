1bb7cce95d6ddf5e3dcbbbe38c9c2c2a2c920863
William Iommi <william.iommi@gmail.com>
2022-01-04 02:34:14 +0100

    phone frame removed size option

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -1,10 +1,9 @@
 import { memo } from "react";
 
-const PhoneFrame = ({ children, pro }) => {
-  const frameSize = pro ? "h-[926px] w-[98%] max-w-[428px]" : "h-[712px] w-[350px]";
+const PhoneFrame = ({ children }) => {
   return (
     <div
-      className={`${frameSize} relative overflow-hidden border-[14px] border-black bg-black rounded-[60px] pb-5 ring ring-orange-400 blur-0`}
+      className={`h-[926px] w-[98%] max-w-[428px] relative overflow-hidden border-[14px] border-black bg-black rounded-[60px] pb-5 ring ring-orange-400 blur-0`}
     >
       <img
         src="/bg.png"
