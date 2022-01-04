d0661f22db04e35eb971fb29962be99a647c34af
William Iommi <william.iommi@gmail.com>
2022-01-04 01:48:08 +0100

    minor

--- src/components/icons/Lens.jsx
+++ src/components/icons/Lens.jsx
@@ -2,20 +2,22 @@ import { memo } from "react";
 
 const Lens = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-5 w-5 text-gray-500"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
+    <span className="inline-block w-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-4 text-gray-500"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </span>
   );
 };
 
--- src/lib/variants.js
+++ src/lib/variants.js
@@ -6,7 +6,7 @@ const closeIconVariant = {
 };
 
 export const appLibraryVariants = {
-  open: { opacity: 1, scale: 1, transition: { duration: 0.199, ease: "linear" } },
+  open: { opacity: 1, scale: 1, transition: { duration: 0.2, ease: "linear" } },
   close: {
     opacity: 0,
     scale: 0.8,
@@ -20,12 +20,12 @@ export const folderDetailVariants = {
     y: 0,
     opacity: 1,
     scale: 1,
-    transition: { delay: 0, ease: "linear", duration: 0.199 },
+    transition: { delay: 0, ease: "linear", duration: 0.2 },
   },
   close: {
     opacity: 0,
     scale: 0,
-    transition: { delay: 0, ease: "linear", duration: 0.199 },
+    transition: { delay: 0, ease: "linear", duration: 0.2 },
   },
 };
 
@@ -33,7 +33,7 @@ export const nestedFolderVariants = {
   open: { scale: 1.5, transition: { ease: "linear", duration: 0.2 } },
   close: {
     scale: 1,
-    transition: { ease: "linear", duration: 0.199, delay: 0 },
+    transition: { ease: "linear", duration: 0.2, delay: 0 },
   },
 };
 
@@ -73,34 +73,61 @@ export const iconVariants = [
 ];
 
 export const folderListVariants = {
-  open: { opacity: 1, pointerEvents: "auto" },
-  close: { opacity: 0, pointerEvents: "none" },
+  open: {
+    opacity: 1,
+    pointerEvents: "auto",
+    transition: { ease: "linear", duration: 0.2 },
+  },
+  close: {
+    opacity: 0,
+    pointerEvents: "none",
+    transition: { ease: "linear", duration: 0.2 },
+  },
 };
 
 export const inputSearchBlockVariants = {
-  close: { width: "100%", transition: { ease: "linear", duration: 0.199, delay: 0.28 } },
-  open: { width: "88%", transition: { ease: "linear", duration: 0.199 } },
+  close: {
+    width: "100%",
+    transition: { ease: "linear", duration: 0.2, delay: 0 },
+  },
+  open: { width: "88%", transition: { ease: "linear", duration: 0.2 } },
 };
 
 export const inputSearchWrapperVariants = {
   close: {
     left: "50%",
     x: "-50%",
-    transition: { ease: "linear", duration: 0.199 },
+    transition: { ease: "linear", duration: 0.2, delay: 0 },
   },
   open: {
     left: "10px",
     x: 0,
-    transition: { ease: "linear", duration: 0.199 },
+    transition: { ease: "linear", duration: 0.2 },
   },
 };
 
 export const inputSearchVariants = {
-  close: { width: "76px", transition: { ease: "linear", duration: 0, delay: 0 } },
-  open: { width: "100%", transition: { ease: "linear", duration: 0, delay: 0.199 } },
+  close: {
+    width: "76px",
+    transition: { ease: "linear", duration: 0, delay: 0 },
+  },
+  open: {
+    width: "100%",
+    transition: { ease: "linear", duration: 0, delay: 0.2 },
+  },
 };
 
 export const inputSearchCancelVariants = {
-  close: { opacity: 0, right: 0, x: "150%", transition: { ease: "linear", duration: 0.199 } },
-  open: { opacity: 1, right: 0, x: 10, transition: { ease: "linear", duration: 0.199 } },
+  close: {
+    opacity: 0,
+    right: 0,
+    x: "150%",
+    transition: { ease: "linear", duration: 0.2 },
+  },
+  open: {
+    opacity: 1,
+    right: 0,
+    x: 10,
+    transition: { ease: "linear", duration: 0.2 },
+  },
 };
